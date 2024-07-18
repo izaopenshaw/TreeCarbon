44c60d11d566536f8a73a8711960ec991feffaee
izaopenshaw <halfprice95@gmail.com>
2024-07-18 16:40:01 +0100

    f

--- R/example.R
+++ R/example.R
@@ -6,7 +6,7 @@ vol <- 50    # m^3
 
 #==========Tariff Number===========
 #==Method A: felled tree
-tariff_vol_area(vol, dbh)
+#tariff_vol_area(vol, dbh)
 
 #==Method B: broadleaf
 # Lookup species code in
